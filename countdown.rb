855dde9f1be4f0b02219320eeaaea4a181e533ea
Andrew Waller <andrewhwaller@gmail.com>
2019-03-17 00:54:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,10 @@
 #write your code here
 
 def countdown(number)
-  while integer > 0 do
+  while integer > 0
     puts "#{number} SECOND(S)"
     number -=
+    
   end
   if number == 0
     puts "HAPPY NEW YEAR!"
